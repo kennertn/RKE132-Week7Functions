fa64598a7c5e6682bc343ce6ddf8f140d72906d9
kennert nurja <nurjakennert@gmail.com>
2024-11-28 20:34:33 +0200

    create new app

---
+++ ArgumentsAndParameters/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection.Metadata;
+
+Console.WriteLine("Enter something:");
+string useinput = Console.ReadLine();
+
+PrintAnyWord(UserInput);
+
+
+static void PrintAnyWord(string anyString)
+{
+    anyString = anyString.ToUpper();
+    for (int i = 0; i < 5; i++) ;
+    {
+        Console.WriteLine(anyString);
+    }
+}
